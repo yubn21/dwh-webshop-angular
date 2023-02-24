5f795afff789e57790c66486719b3afd76e0eb2b
yubn21 <125863226+yubn21@users.noreply.github.com>
2023-02-24 14:03:37 +0100

    7 files modified, 3 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -24,7 +24,7 @@ export class CartComponent {
   onSubmit(): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
+    window.alert('Your order has been submitted');
     this.checkoutForm.reset();
   }
 }
